e7741b6bb7a60612a87fe2a59fb5ec4c84edece0
jeisongomez <jeison_gomez1997@hotmail.com>
2018-03-03 20:50:02 -0500

    commic

---
+++ app.js
@@ -0,0 +1,5 @@
+var app = angular.module('adminPanel', []);
+app.controller('namesController', function($scope,$http) {
+    $http.get('http://www.titandesarrollo.com/_api/movimientos')
+    .then(function (response) {$scope.names = response.data;});
+});
